02cb7e694fc91b77b9229f170af70e87709c18e3
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-17 18:10:32 +0600

    first activity

---
+++ module1/lesson 1/activity.py
@@ -0,0 +1,8 @@
+print("Welcome to python programming!!!")
+print("Hello, everyone! My name is sheikh tahmeed. ")
+
+#this is a comment
+print("Today is my first day of Python.")
+
+#\n -> new line
+print("So far i am enjoyin the lesson. \ni hope to learn more!")
