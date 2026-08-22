8a6de9846f78e9578766bb3927f7b26e964883e2
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-22 12:10:48 +0600

    homework 1

---
+++ homework 1/activity.py
@@ -0,0 +1,22 @@
+print("=== My Daily Routine ===")
+print("Hii! I'm Sheikh Tahmeed Tamim, and this is what my typical day looks like.")
+#\n -> new line
+print(" Morning:")
+print("at first i Wake up and have breakfast.")
+print("than i get ready for the day.")
+print("i study for my AS exams, especially Maths, Physics and Chemistry.")
+#\n -> new line
+print(" Afternoon:")
+print("Continue studying and doing practice questions.")
+print("i take breaks so I don't burn out.")
+print("than i have lunch and relax for a while.")
+#\n -> new line
+print(" Evening:")
+print("Work out and do exercises.")
+print("Spend some time playing games with my friends.")
+print("Finish any studying I still have left.")
+#\n -> new line
+print(" Night:")
+print("i watch some episodes of my favourite series.")
+print("i review what I studied during the day.")
+print("than i eat my dinner and get ready for bed.")
