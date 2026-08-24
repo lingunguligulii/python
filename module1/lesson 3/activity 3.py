27028309da0fce3d9c0ef690f940a395ceec1ee7
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-24 15:25:39 +0600

    lesson 3

---
+++ module1/lesson 3/activity 3.py
@@ -0,0 +1,14 @@
+age = 16
+total_marks = 122.35
+name = "Tahmeed"
+is_student = True
+
+# Finding out the types 
+print(type(age))
+print(type(total_marks))
+print(type(name))
+print(type(is_student))
+
+#Typecasting -> converting value from one type to another
+total_marks = int(total_marks)
+print(f"The value of total marks: {total_marks} and type: {type(total_marks)}")
