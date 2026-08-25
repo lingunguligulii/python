4b6e9a6532791f520143a537610dd2e84b940a2c
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-25 23:22:05 +0600

    homework 2

---
+++ module1/lesson 2/homework.py
@@ -0,0 +1,14 @@
+patrick = "19th june"
+print(f"Patrick's birthday is on {patrick}. ")
+
+lisbon = "24th september"
+print(f"Lisbon's birthday is on {lisbon}. ")
+
+rigsby = "17th may"
+print(f"Rigsby's birthday is on {rigsby}. ")
+
+grace = "31st august"
+print(f"Grace's birthday is on {grace}. ")
+
+cho = "21st december"
+print(f"cho's birhtday is on {cho}. ")
