6e0bd6948070da18de03d350d426fcdf8d2c8ae0
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-26 16:23:27 +0600

    lesson 4

---
+++ module1/lesson 4/activity 1.py
@@ -0,0 +1,5 @@
+item_price = float(input("Enter the price of ur item:"))
+item_quantity = int(input("Enter the quantity:"))
+
+total_bill = item_price * item_quantity
+print(f"Your total bill: {total_bill} BDT")
