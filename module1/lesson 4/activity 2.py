e4dc1feb383758bb1b10b1a57e52cf23aa796e0a
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-26 16:31:01 +0600

    lesson 4

---
+++ module1/lesson 4/activity 2.py
@@ -0,0 +1,11 @@
+tree1 = 94
+tree2 = 45
+tree3 = 11
+tree4 = 92
+tree5 = 67
+
+sum_of_heights = tree1 + tree2 + tree3 + tree4 + tree5
+print(f"Total height of all the trees: {sum_of_heights} inch")
+
+average = sum_of_heights//5
+print(f"Average height of a tree: {average} inch.")
