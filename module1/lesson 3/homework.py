f75e5fed059f25936706f99e1f59a6605f67aec6
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-26 23:37:05 +0600

    lesson 3

---
+++ module1/lesson 3/homework.py
@@ -0,0 +1,10 @@
+name = "John"
+birth_year = 2009
+hobbies = "football, gaming, reading"
+
+print("Reversed name:", name[::-1])
+
+age = 2026 - birth_year
+print("Age:", age)
+
+print("Hobbies:", hobbies.upper())
