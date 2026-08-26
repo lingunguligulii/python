32c7aa97e4263eaf562c1e897e5bd11a595c80ed
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-26 16:40:58 +0600

    lesson 4

---
+++ module1/lesson 4/activity 3.py
@@ -0,0 +1,7 @@
+term1_marks = 1290
+term2_marks = 1340
+
+print("Better than last term? ->", term2_marks > term1_marks)
+print("Same as the last term? ->", term2_marks == term1_marks)
+print("Worse than the last term? ->", term2_marks < term1_marks)
+print("At least as good as the last term? -> ", term2_marks >= term1_marks)
