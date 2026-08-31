83f7e7cc1f324431117ea6551ff3d41ce67d1ca6
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-31 17:52:33 +0600

    lesson 5

---
+++ module1/lesson 5/activity 3.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks (out of 100):"))
+
+if marks >= 80:
+    print("A+")
+elif marks >= 70:
+    print("A")
+elif marks >= 60:
+    print("B")
+elif marks >= 50:
+    print("C")
+elif marks >= 45:
+    print("D")
+else:
+    print("F")
