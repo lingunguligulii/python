9f33d4fd80ffcf80832a29eef9f8d502d70d133c
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-08-31 17:32:02 +0600

    lesson 5

--- module1/lesson 4/homework.py
+++
@@ -1,5 +0,0 @@
-number = int(input("Enter a number: "))
-
-square_root = number ** 0.5
-
-print("The square root is:", square_root)
---
+++ module1/lesson 5/activity 1.py
@@ -0,0 +1,7 @@
+number = int(input("enter the number:"))
+print(f"Number entered is {number}")
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
