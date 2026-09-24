e992f0285c751dc29f6ad990e94f3c9bd1c24d58
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-09-24 16:54:27 +0600

    lesson 1

---
+++ module 2/lesson 1/activity 3.py
@@ -0,0 +1,34 @@
+word = "PYTHON"
+for letter in word:
+    print(letter)
+
+print()
+word = "CODING"
+for letter in word:
+    print("Character:", letter)
+
+print()
+word = "PYTHON"
+for letter in word:
+    print(letter, end="-")
+
+print()
+message = "HELLO"
+for char in message:
+    print(f"[{char}]", end="")
+
+print()
+word = "programming"
+for letter in word.capitalize():
+    print(letter, end=" ")
+
+print()
+word = "programming"
+for letter in word.upper():
+    print(letter, end=" ")
+
+print()
+word = "PRogRAMming"
+for letter in word.lower():
+    print(letter, end=" ")
+
