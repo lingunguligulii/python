50e2071cc4a61041a5b3caaaa95c9ef965907c76
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-09-24 16:44:22 +0600

    lesson 1

---
+++ module 2/lesson 1/activity 2.py
@@ -0,0 +1,15 @@
+for number in range(5):
+    print(number)
+
+print()
+for number in range(2, 7):
+    print(number)
+
+print()
+
+for number in range(2, 10, 2):
+    print(number)
+
+print()
+for number in range(10, 0, -1):
+    print(number)
