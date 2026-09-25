e81a3761806cc6688b1203a54c45e246fb3368d1
lingunguligulii <sheikhtahmeed3@gmail.com>
2026-09-25 16:29:15 +0600

    lesson 2

---
+++ module 2/lesson 2/activity 1.py
@@ -0,0 +1,13 @@
+number = 1
+while number <= 10:
+    print(number)
+    number += 1
+
+print("loop finished!")
+
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+
+print("loop finished!")
